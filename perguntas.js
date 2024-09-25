1c4a56661c2ebce156b50a2fe452a67d3ef6a1b4
Matheus Luciano <148216727+Teteu-Luciano@users.noreply.github.com>
2024-09-25 19:01:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'ciencia',
+    'o corpo humano é constituido por?',
+    'celulas e orgaos'
+)
+
+criaCartao(
+    'Geografia',
+    'onde ocorreu o grito da independencia do Brasil?',
+    ' às margens do rio Ipiranga'
+)
+
+criaCartao(
+    'Historia',
+    'em proclamou a independencia no Brasil?',
+    'Don Pedro primeiro  ' 
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'qual a tradução de  shortness of breathe ?',
+    'falta de ar'
+)
